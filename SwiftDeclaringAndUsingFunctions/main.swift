125dd90b67892a7539da8fd0af61f395b69a0e11
MacStudent <macstudent@126-iMac26.local>
2019-02-11 18:56:34 -0500

    labels

--- SwiftDeclaringAndUsingFunctions/main.swift
+++ SwiftDeclaringAndUsingFunctions/main.swift
@@ -10,3 +10,36 @@ import Foundation
 
 print("Hello, World!")
 
+func hello()  {
+    print("hello")
+
+}
+
+func sum(a:Int, b:Int)  {
+    let c = a+b
+    print("Sum:\(c)")
+    
+}
+
+sum(a: 10, b: 20) // with default label
+//sum(b: 20, a: 40)// cant access in this way
+
+func sum(of a: Float,and b: Float){// with user defined  label
+    
+    let c = a+b
+    print("Sum: -F \(c)")
+    
+}
+
+//sum(a: 10.5, b: 20.0)
+
+sum(of: 10.5, and: 20.0)
+
+func subtraction(_ a: Int ,_ b: Int) /// without label
+{
+    
+    let c = a-b
+    print("subtraction:  \(c)")
+    
+}
+subtraction(20, 10)
